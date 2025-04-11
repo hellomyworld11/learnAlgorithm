5210f06164f3bb003d8c5290428d225dd3a79648
xupan <2495526723@qq.com>
2025-04-11 11:02:01 +0800

    add

--- datastructure/new/dynamicprograming/minpath_sum.cpp
+++ datastructure/new/dynamicprograming/minpath_sum.cpp
@@ -1,6 +1,5 @@
 #include "minpath_sum.h"
 
-
 int minPathSumDFS(vector<vector<int>> &grid, int i, int j)
 {
 	if (i == 0 && j == 0)
@@ -19,6 +18,87 @@ int minPathSumDFS(vector<vector<int>> &grid, int i, int j)
 	return (minpath != INT_MAX) ? minpath + grid[i][j] : INT_MAX;
 }
 
+int minPathSumDFSMem(vector<vector<int>>& grid, vector<vector<int>>& mem, int i, int j)
+{
+	if (i == 0 && j == 0)
+	{
+		return grid[0][0];
+	}
+
+	if (i < 0 || j < 0)
+	{
+		return INT_MAX;
+	}
+
+	if (mem[i][j] != -1)
+	{
+		return mem[i][j];
+	}
+	int up = minPathSumDFSMem(grid, mem, i-1, j);
+	int left = minPathSumDFSMem(grid, mem, i, j - 1);
+	int minpath = min(up, left);
+	mem[i][j] = minpath != INT_MAX ? minpath + grid[i][j] : INT_MAX;
+	return mem[i][j];
+}
+
+int minPathSumDP(vector<vector<int>> &grid, int row, int col)
+{
+	if (row == 0 && col == 0)
+	{
+		return grid[0][0];
+	}
+
+	int rows = grid.size();
+	int cols = grid[0].size();
+
+	vector<vector<int>> dps(rows, vector<int>(cols));
+	dps[0][0] = grid[0][0];
+
+	//首行
+	for (int i = 1; i < cols; i++)
+	{
+		dps[0][i] = dps[0][i - 1] + grid[0][i];
+	}
+
+	//首列
+	for (int j = 1; j < rows; j++)
+	{
+		dps[j][0] = dps[j-1][0] + grid[j][0];
+	}
+
+	//其余需要比较最小路径
+	for (int i = 1; i < rows; i++)
+	{
+		for (int j = 1; j < cols; j++)
+		{
+			dps[i][j] = min(dps[i - 1][j], dps[i][j - 1]) + grid[i][j];
+		}
+	}
+
+	return dps[row][col];
+}
+
+int minPathSumDPComp(vector<vector<int>> &grid, int row, int col)
+{
+	int n = grid.size(), m = grid[0].size();
+	// 初始化 dp 表
+	vector<int> dp(m);
+	// 状态转移：首行
+	dp[0] = grid[0][0];
+	for (int j = 1; j < m; j++) {
+		dp[j] = dp[j - 1] + grid[0][j];
+	}
+	// 状态转移：其余行
+	for (int i = 1; i < n; i++) {
+		// 状态转移：首列
+		dp[0] = dp[0] + grid[i][0];
+		// 状态转移：其余列
+		for (int j = 1; j < m; j++) {
+			dp[j] = min(dp[j - 1], dp[j]) + grid[i][j];
+		}
+	}
+	return dp[m - 1];
+
 void testMinPath()
 {
 	vector<vector<int>> grid = { { 1, 3, 1, 5 },{ 2, 2, 4, 2 },{ 5, 3, 2, 1 },{ 4, 3, 5, 2 } };
@@ -26,5 +106,8 @@ void testMinPath()
 
 	// 暴力搜索
 	int res = minPathSumDFS(grid, n - 1, m - 1);
-	cout << "从左上角到右下角的最小路径和为 " << res << endl;
+	cout << "暴力搜索 从左上角到右下角的最小路径和为 " << res << endl;
+
+	res = minPathSumDP(grid, n - 1, m - 1);
+	cout << "动态规划 从左上角到右下角的最小路径和为 " << res << endl;
 }
--- datastructure/new/dynamicprograming/minpath_sum.h
+++ datastructure/new/dynamicprograming/minpath_sum.h
@@ -2,9 +2,16 @@
 #include "../util.h"
 
 
-/* 最小路径和：暴力搜索 */
+/* 最小路径和：暴力搜索 存在重叠子问题  */
 int minPathSumDFS(vector<vector<int>> &grid, int i, int j);
 
+// 最小路径和：记忆化搜索 
+int minPathSumDFSMem(vector<vector<int>>& grid, vector<vector<int>>& mem, int i, int j);
 
+/* 最小路径和：动态规划 */
+int minPathSumDP(vector<vector<int>> &grid, int row, int col);
+
+/* 最小路径和：动态规划 空间优化 */
+int minPathSumDPComp(vector<vector<int>> &grid, int row, int col);
 
 void testMinPath();
